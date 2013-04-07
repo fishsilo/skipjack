ceeece610643bfa68bae9628701595db998ac0c1
Corry Haines <tabletcorry@gmail.com>
2013-04-07 00:22:31 -0700

    Trigger the puppet manifest on all machines

--- modules/truth/manifests/enforcer.pp
+++ modules/truth/manifests/enforcer.pp
@@ -1,9 +1,11 @@
 class truth::enforcer {
-  
-  # Now conditionally include things based on properties and facts
-  if has_role("loadbalancer") {
-    include haproxy
-    notice("I am a loadbalancer")
-  }
+
+    include puppet
+
+    # Now conditionally include things based on properties and facts
+    if has_role("loadbalancer") {
+        include haproxy
+        notice("I am a loadbalancer")
+    }
 
 }
