ac237fa9fa09c2192c7a81c8983750cd9d8c0eca
Corry Haines <tabletcorry@gmail.com>
2013-04-15 19:48:53 -0700

    Modify haproxy to do nothing for now

--- modules/haproxy/manifests/init.pp
+++ modules/haproxy/manifests/init.pp
@@ -1,10 +1,4 @@
 class haproxy::package {
-    package {
-        "haproxy-1.5":
-            ensure => installed,
-            provider => dpkg
-            source => "https://s3-us-west-2.amazonaws.com/com.fishsilo.skipjack/haproxy-15_1.5-dev18_i386.deb";
-    }
 }
 
 class haproxy {
